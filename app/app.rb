1230d729601ed75decdfb0350b4cb7987016b3ce
Lucy Fang <lucyfang2@gmail.com>
2015-09-22 13:51:35 +0100

    Added creating a link page

--- app/app.rb
+++ app/app.rb
@@ -2,20 +2,34 @@ require 'sinatra/base'
 require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
-  # ... app code here ...
 
-  get '/' do
-    erb :index
-  end
+  # get '/' do
+  #   erb :index
+  # end
 
   get '/links' do
     @links = Link.all
     erb :'links/index'
   end
 
+  get '/links/new' do
+    erb :'links/create_links'
+  end
+
+  post '/links' do
+    # session[:url] = params[:url]
+    # session[:title] = params[:title]
+    Link.create(url: params[:url], title: params[:title])
+    redirect '/links'
+  end
+
+  # get '/links/' do
+  #
+  # end
+
 
     # start the server if ruby file executed directly
-    run! if app_file == $0
+  run! if app_file == $0
 
 
 end
