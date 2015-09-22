81334075b70f1ecccc0a65a5d5509c907a259c4f
Matt Paul <mattpaul001@icloud.com>
2015-09-22 15:20:53 +0100

    Data cleaner

--- Gemfile
+++ Gemfile
@@ -10,4 +10,5 @@ group :development, :test do
  gem 'rspec-sinatra'
  gem 'data_mapper'
  gem 'dm-postgres-adapter'
+ gem 'database_cleaner'
 end
--- spec/features/creating_links_spec.rb
+++ spec/features/creating_links_spec.rb
@@ -8,12 +8,16 @@ feature 'Creating links' do
     fill_in 'url',   with: 'http://www.zombo.com/'
     fill_in 'title', with: 'This is Zombocom'
     click_button 'Create link'
-
     # we expect to be redirected back to the links page
     expect(current_path).to eq '/links'
-
     within 'ul#links' do
       expect(page).to have_content('This is Zombocom')
     end
   end
+
+
+  scenario 'there are no links in the database at the start of the test' do
+    expect(Link.count).to eq 0
+  end
+
 end
